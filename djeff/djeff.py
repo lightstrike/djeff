c09be7e403e7980ef4a7235a9368bbbeac71b3eb
Will Plaehn <will@lastmileretail.com>
2015-11-11 21:18:14 -0500

    middleware approach with functioning parser. needs better djeffing logic

--- djeff/djeff.py
+++ djeff/djeff.py
@@ -1,1 +1,28 @@
-# -*- coding: utf-8 -*-
+from HTMLParser import HTMLParser
+
+
+def reconstruct_attrs(attrs):
+    tag_string = ''
+    for attr in attrs:
+        tag_string += (attr[0] + '=' + attr[1] + ' ')
+    return tag_string.strip()
+
+
+class DjeffParser(HTMLParser):
+    text = ''
+
+    def handle_starttag(self, tag, attrs):
+        self.text += ('<%s %s>' % (tag, reconstruct_attrs(attrs)))
+
+    def handle_endtag(self, tag):
+        self.text += ('</%s>' % tag)
+
+    def handle_data(self, data):
+        data = data.replace(' G', ' dj')
+        self.text += data
+
+
+def djeff(string):
+    parser = DjeffParser()
+    parser.feed(string)
+    return parser.text
