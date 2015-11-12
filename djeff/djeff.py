6becf566998e8e57d86d1792b6676b67f44877ec
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-11-11 22:48:23 -0500

    template engine approach

--- djeff/djeff.py
+++ djeff/djeff.py
@@ -1,4 +1,27 @@
-from HTMLParser import HTMLParser
+from django.template.backends.django import DjangoTemplates, Template
+from django.template.engine import _dirs_undefined
+from html.parser import HTMLParser
+
+
+class DjeffTemplates(DjangoTemplates):
+    def get_template(self, template_name, dirs=_dirs_undefined):
+        return DjeffTemplate(self.engine.get_template(template_name, dirs))
+
+
+class DjeffTemplate(Template):
+    def render(self, context=None, request=None):
+        rendered_context = super().render(context, request)
+        return djeffify(rendered_context)
+
+
+def djeffify(rendered_string):
+    """
+    This function contains the core logic for a
+    middleware, template tag or Template engine approach
+    """
+    parser = DjeffParser()
+    parser.feed(rendered_string)
+    return parser.dhtml
 
 
 def reconstruct_attrs(attrs):
@@ -9,20 +32,21 @@ def reconstruct_attrs(attrs):
 
 
 class DjeffParser(HTMLParser):
-    text = ''
+    def __init__(self, *, convert_charrefs=True):
+        super().__init__(convert_charrefs)
+        self.dhtml = ''
 
     def handle_starttag(self, tag, attrs):
-        self.text += ('<%s %s>' % (tag, reconstruct_attrs(attrs)))
+        self.dhtml += '<{} {}>'.format(tag, reconstruct_attrs(attrs))
 
     def handle_endtag(self, tag):
-        self.text += ('</%s>' % tag)
+        self.dhtml += '</{}>'.format(tag)
 
     def handle_data(self, data):
-        data = data.replace(' G', ' dj')
-        self.text += data
+        """
+        FIXME: Add more functionality!
+        """
+        if data.strip():
+            data = "d{}".format(data)
+        self.dhtml += data
 
-
-def djeff(string):
-    parser = DjeffParser()
-    parser.feed(string)
-    return parser.text
